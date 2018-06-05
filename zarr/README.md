9094e109655fbf51089771b33523b44f96e20b24
Tom White <tom@cloudera.com>
2018-06-05 15:25:34 +0100

    Refactor

--- zarr/README.md
+++ zarr/README.md
@@ -1,4 +1,16 @@
-```bash
-virtualenv venv
-source venv/bin/activate
-```
+# Zarr and Spark
+
+Experiments to read and write Zarr files from Spark.
+
+## Representation
+
+Spark's RowMatrix is used. Assume that the row lengths are small enough that the entire row fits into a Zarr chunk; in
+other words, the chunk width is the same as the row width.
+
+## Matrix operations
+
+* Add or remove columns. Adjust chunk width. Easy to handle since row partitioning does not change.
+* Add or remove rows. Changes row partitioning. Simplest way to handle is to shuffle with the chunk as the key. May
+be able to be more sophisticated with a clever Spark coalescer that can read from other partitions.
+* Matrix multiplication. Multiplying by a matrix on the right preserves partitioning, so only chunk width needs to
+change.
--- zarr/local.py
+++ zarr/local.py
@@ -1,66 +1,62 @@
+# virtualenv venv
+# source venv/bin/activate
+# pip install zarr
 # export PYSPARK_PYTHON=$(pwd)/venv/bin/python
 # ~/sw/spark-2.2.1-bin-hadoop2.7/bin/pyspark
 
-import zarr
-
+import math
 import numpy as np
-
-# Write to a local file
-z = zarr.open('data/mini.zarr', mode='w', shape=(4, 3),
-               chunks=(2, 3), dtype='i4', compressor=None)
-z[:, 0] = np.arange(4)
-z[:, 1] = np.arange(4, 8)
-z[:, 2] = np.arange(8, 12)
-
-# Write to a local file
-z = zarr.open('data/mini.zarr', mode='w', shape=(3, 5),
-              chunks=(2, 5), dtype='f8', compressor=None)
-z[0, :] = [0.0, 1.0, 0.0, 3.0, 0.0]
-z[1, :] = [2.0, 0.0, 3.0, 4.0, 5.0]
-z[2, :] = [4.0, 0.0, 0.0, 6.0, 7.0]
-
-# Read in parallel
-# To keep things simple each task reads a single chunk
-
 import zarr
-import numpy as np
 
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.linalg.distributed import RowMatrix
 from pyspark.sql.types import *
 
-spark = SparkSession \
-    .builder \
-    .appName("zarr-demo") \
-    .getOrCreate()
-sc = spark.sparkContext
+# Utility functions for reading and writing a Zarr array chunk; these are designed to be run as Spark tasks.
+# Currently it is assumed that all arrays are 2D and that rows are not chunked. Also each task reads/writes a single chunk.
 
-chunks = sc.parallelize(((0, 0), (1, 0)), 2)
-
-input_file = '/Users/tom/workspace/hdf5-experiments/zarr/data/mini.zarr'
-output_file = '/Users/tom/workspace/hdf5-experiments/zarr/data/svd.zarr'
-
-def noop(x):
-    pass
-
-def pr(x):
-    print(x, type(x))
-
-def read_chunk(x):
+def get_chunk_indices(za):
     """
-    Read a zarr chunk specified by coordinates x=(a,b) (2D-only).
+    Return all the indices (coordinates) for the chunks in a zarr array, even empty ones.
     """
-    z = zarr.open(input_file, mode='r')
-    chunk_size = z.chunks
-    return z[chunk_size[0]*x[0]:chunk_size[0]*(x[0]+1),chunk_size[1]*x[1]:chunk_size[1]*(x[1]+1)]
+    shape = za.shape
+    chunk_size = za.chunks
+    return [(i, j) for i in range(int(math.ceil(float(shape[0])/chunk_size[0])))
+            for j in range(int(math.ceil(float(shape[1])/chunk_size[1])))]
 
-chunks.map(read_chunk).foreach(pr)
+def read_chunk(file):
+    """
+    Return a function to read a chunk by coordinates from the given file.
+    """
+    def read_one_chunk(chunk_index):
+        """
+        Read a zarr chunk specified by coordinates chunk_index=(a,b).
+        """
+        z = zarr.open(file, mode='r')
+        chunk_size = z.chunks
+        return z[chunk_size[0]*chunk_index[0]:chunk_size[0]*(chunk_index[0]+1),chunk_size[1]*chunk_index[1]:chunk_size[1]*(chunk_index[1]+1)]
+    return read_one_chunk
+
+def write_chunk(file):
+    """
+    Return a function to write a chunk by index to the given file.
+    """
+    def write_one_chunk(index_arr):
+        """
+        Write a partition index and numpy array to a zarr store. The array must be the size of a chunk, and not
+        overlap other chunks.
+        """
+        index, arr = index_arr
+        z = zarr.open(file, mode='r+')
+        chunk_size = z.chunks
+        z[chunk_size[0]*index:chunk_size[0]*(index+1),:] = arr
+    return write_one_chunk
 
 def ndarray_to_vector(arr):
     """
-    Convert a numpy array to a Spark Vector
+    Convert a numpy array to a Spark Vector.
     """
-    return map(lambda row : Vectors.dense(row), arr.tolist())
+    return [Vectors.dense(row) for row in arr.tolist()]
 
 def vectors_to_ndarray(index, vs):
     """
@@ -68,30 +64,52 @@ def vectors_to_ndarray(index, vs):
     """
     return [(index, np.array([v.toArray() for v in vs]))]
 
-vec = chunks.map(read_chunk).flatMap(ndarray_to_vector)
+
+# Demo
+
+input_file = '/tmp/mini.zarr'
+output_file = '/tmp/svd.zarr'
+
+# Write a small array to a local file
+z = zarr.open(input_file, mode='w', shape=(3, 5),
+              chunks=(2, 5), dtype='f8', compressor=None)
+z[0, :] = [0.0, 1.0, 0.0, 3.0, 0.0]
+z[1, :] = [2.0, 0.0, 3.0, 4.0, 5.0]
+z[2, :] = [4.0, 0.0, 0.0, 6.0, 7.0]
+
+# Have a look at the data
+z[:]
+
+# Use Spark to read and write
+
+spark = SparkSession \
+    .builder \
+    .appName("zarr-demo") \
+    .getOrCreate()
+sc = spark.sparkContext
+
+chunk_indices = sc.parallelize(get_chunk_indices(z), 2)
+
+def pr(x):
+    print(x)
+
+# Read the entire array. Each chunk is printed by a separate task.
+chunk_indices.map(read_chunk(input_file)).foreach(pr)
+
+# Convert the array to a Spark Vector, then run SVD, and convert the result back to an array.
+vec = chunk_indices.map(read_chunk(input_file)).flatMap(ndarray_to_vector)
 mat = RowMatrix(vec)
 svd = mat.computeSVD(2, True)
-
 u = svd.U # U has original number of rows (3) and projected number of cols (2)
-u.rows.mapPartitions(vectors_to_ndarray).foreach(pr)
 
-# Create a new Zarr file, but only write metadata
-z = zarr.open(output_file, mode='w', shape=(3, 2),
+# Create a new Zarr file, but only write metadata.
+# N.B. no good way currently to find chunk size - have to hardcode it here.
+z = zarr.open(output_file, mode='w', shape=(u.numRows(), u.numCols()),
               chunks=(2, 2), dtype='f8', compressor=None)
-
-# Write each partition in a task
-def write_chunk(x):
-    """
-    Write a partition index and numpy array to a zarr store. The array must be the size of a chunk, and not
-    overlap other chunks
-    """
-    index, arr = x
-    z = zarr.open(output_file, mode='r+')
-    chunk_size = z.chunks
-    z[chunk_size[0]*index:chunk_size[0]*(index+1),:] = arr
-
-u.rows.mapPartitionsWithIndex(vectors_to_ndarray).foreach(write_chunk)
+# Write the entire array. Each chunk is written by a separate task.
+u.rows.mapPartitionsWithIndex(vectors_to_ndarray).foreach(write_chunk(output_file))
 
 # Read back locally
 z = zarr.open(output_file, mode='r')
 z[:]
+
