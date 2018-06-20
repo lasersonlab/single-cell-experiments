f1b0bc692eb58b895629b251804190b0e311742c
Tom White <tom@cloudera.com>
2018-06-20 10:53:15 +0100

    Add code to convert 10x to anndata

---
+++ scanpy/local-10x-to-anndata.py
@@ -0,0 +1,11 @@
+# python3 -m venv venv3
+# source venv3/bin/activate
+# pip install numpy anndata
+# export PYSPARK_PYTHON=$(pwd)/venv3/bin/python
+# ~/sw/spark-2.2.1-bin-hadoop2.7/bin/pyspark
+
+import scanpy
+
+adata = scanpy.api.read_10x_h5('/Users/tom/workspace/hdf5-java-cloud/files/1M_neurons_filtered_gene_bc_matrices_h5.h5')
+#adata = scanpy.api.read_10x_h5('/Downloads/1M_neurons_filtered_gene_bc_matrices_h5.h5')
+adata.write('data/10x.h5ad')
