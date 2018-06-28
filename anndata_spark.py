5d4a79d7f17355900d874fcdd46733c1d65ec0d8
Tom White <tom.e.white@gmail.com>
2018-06-28 14:33:58 +0100

    Refactoring to remove duplication

--- anndata_spark.py
+++ anndata_spark.py
@@ -15,13 +15,16 @@ def get_chunk_indices(shape, chunk_size):
     return [(i, j) for i in range(int(math.ceil(float(shape[0])/chunk_size[0])))
             for j in range(int(math.ceil(float(shape[1])/chunk_size[1])))]
 
+def read_adata_chunk(adata, chunks, chunk_index):
+    return adata.X[chunks[0]*chunk_index[0]:chunks[0]*(chunk_index[0]+1),chunks[1]*chunk_index[1]:chunks[1]*(chunk_index[1]+1)]
+
 def read_chunk_csv(csv_file, chunk_size):
     """
     Return a function to read a chunk by coordinates from the given file.
     """
     def read_one_chunk(chunk_index):
         adata = ad.read_csv(csv_file)
-        return adata.X[chunk_size[0]*chunk_index[0]:chunk_size[0]*(chunk_index[0]+1),chunk_size[1]*chunk_index[1]:chunk_size[1]*(chunk_index[1]+1)]
+        return read_adata_chunk(adata, chunk_size, chunk_index)
     return read_one_chunk
 
 def read_chunk_zarr(zarr_file, chunk_size):
@@ -30,7 +33,7 @@ def read_chunk_zarr(zarr_file, chunk_size):
     """
     def read_one_chunk(chunk_index):
         adata = ad.read_zarr(zarr_file)
-        return adata.X[chunk_size[0]*chunk_index[0]:chunk_size[0]*(chunk_index[0]+1),chunk_size[1]*chunk_index[1]:chunk_size[1]*(chunk_index[1]+1)]
+        return read_adata_chunk(adata, chunk_size, chunk_index)
     return read_one_chunk
 
 def read_chunk_zarr_gcs(gcs_path, chunk_size, gcs_project, gcs_token):
@@ -42,7 +45,7 @@ def read_chunk_zarr_gcs(gcs_path, chunk_size, gcs_project, gcs_token):
         gcs = gcsfs.GCSFileSystem(gcs_project, token=gcs_token)
         store = gcsfs.mapping.GCSMap(gcs_path, gcs=gcs)
         adata = ad.read_zarr(store)
-        return adata.X[chunk_size[0]*chunk_index[0]:chunk_size[0]*(chunk_index[0]+1),chunk_size[1]*chunk_index[1]:chunk_size[1]*(chunk_index[1]+1)]
+        return read_adata_chunk(adata, chunk_size, chunk_index)
     return read_one_chunk
 
 def write_chunk_zarr(zarr_file):
@@ -88,6 +91,15 @@ class AnnDataRdd:
         self.dtype = dtype # need to store since adata.X is None so can't retrieve dtype from there
 
     @classmethod
+    def _from_anndata(cls, sc, adata, chunk_size, read_chunk_fn):
+        dtype = adata.X.dtype
+        ci = get_chunk_indices(adata.X.shape, chunk_size)
+        adata.X = None # data is stored in the RDD
+        chunk_indices = sc.parallelize(ci, len(ci))
+        rdd = chunk_indices.map(read_chunk_fn)
+        return cls(adata, rdd, dtype)
+
+    @classmethod
     def from_csv(cls, sc, csv_file, chunk_size):
         """
         Read a CSV file as an anndata object (for the metadata) and with the
@@ -96,57 +108,32 @@ class AnnDataRdd:
         redundant and won't scale. This should be improved, possibly by changing anndata.
         """
         adata = ad.read_csv(csv_file)
-        dtype = adata.X.dtype
-        ci = get_chunk_indices(adata.X.shape, chunk_size)
-        adata.X = None # data is stored in the RDD
-        chunk_indices = sc.parallelize(ci, len(ci))
-        rdd = chunk_indices.map(read_chunk_csv(csv_file, chunk_size))
-        return cls(adata, rdd, dtype)
+        return cls._from_anndata(sc, adata, chunk_size, read_chunk_csv(csv_file, chunk_size))
 
     @classmethod
     def from_zarr(cls, sc, zarr_file):
+        """
+        Read a Zarr file as an anndata object (for the metadata) and with the
+        data matrix (X) as an RDD of numpy arrays.
+        """
         adata = ad.read_zarr(zarr_file)
-        dtype = adata.X.dtype
         chunk_size = zarr.open(zarr_file, mode='r')['X'].chunks
-        ci = get_chunk_indices(adata.X.shape, chunk_size)
-        adata.X = None # data is stored in the RDD
-        chunk_indices = sc.parallelize(ci, len(ci))
-        rdd = chunk_indices.map(read_chunk_zarr(zarr_file, chunk_size))
-        return cls(adata, rdd, dtype)
+        return cls._from_anndata(sc, adata, chunk_size, read_chunk_zarr(zarr_file, chunk_size))
 
     @classmethod
     def from_zarr_gcs(cls, sc, gcs_path, gcs_project, gcs_token='cloud'):
+        """
+        Read a Zarr file from GCS as an anndata object (for the metadata) and with the
+        data matrix (X) as an RDD of numpy arrays.
+        """
         import gcsfs.mapping
         gcs = gcsfs.GCSFileSystem(gcs_project, token=gcs_token)
         store = gcsfs.mapping.GCSMap(gcs_path, gcs=gcs)
         adata = ad.read_zarr(store)
-        dtype = adata.X.dtype
         chunk_size = zarr.open(store, mode='r')['X'].chunks
-        ci = get_chunk_indices(adata.X.shape, chunk_size)
-        adata.X = None # data is stored in the RDD
-        chunk_indices = sc.parallelize(ci, len(ci))
-        rdd = chunk_indices.map(read_chunk_zarr_gcs(gcs_path, chunk_size, gcs_project, gcs_token))
-        return cls(adata, rdd, dtype)
+        return cls._from_anndata(sc, adata, chunk_size, read_chunk_zarr_gcs(gcs_path, chunk_size, gcs_project, gcs_token))
 
-    def write_zarr(self, zarr_file, chunks):
-        # write the metadata out using anndata
-        self.adata.write_zarr(zarr_file, chunks)
-        # write X using Spark
-        z = zarr.open(zarr_file, mode='w')
-        shape = (self.adata._n_obs, self.adata._n_vars)
-        z.create_dataset('X', shape=shape, chunks=chunks, dtype=self.dtype)
-        # TODO: the following only works if each partition in the RDD has the same number of rows, which will not be true if there has been any row filtering
-        # TODO: handle this case by doing a shuffle
-        def index_partitions(index, iterator):
-            values = list(iterator)
-            assert len(values) == 1 # 1 numpy array per partition
-            return [(index, values[0])]
-        self.rdd.mapPartitionsWithIndex(index_partitions).foreach(write_chunk_zarr(zarr_file))
-
-    def write_zarr_gcs(self, gcs_path, chunks, gcs_project, gcs_token='cloud'):
-        import gcsfs.mapping
-        gcs = gcsfs.GCSFileSystem(gcs_project, token=gcs_token)
-        store = gcsfs.mapping.GCSMap(gcs_path, gcs=gcs)
+    def _write_zarr(self, store, chunks, write_chunk_fn):
         # write the metadata out using anndata
         self.adata.write_zarr(store, chunks)
         # write X using Spark
@@ -159,7 +146,22 @@ class AnnDataRdd:
             values = list(iterator)
             assert len(values) == 1 # 1 numpy array per partition
             return [(index, values[0])]
-        self.rdd.mapPartitionsWithIndex(index_partitions).foreach(write_chunk_zarr_gcs(gcs_path, gcs_project, gcs_token))
+        self.rdd.mapPartitionsWithIndex(index_partitions).foreach(write_chunk_fn)
+
+    def write_zarr(self, zarr_file, chunks):
+        """
+        Write an anndata object to a Zarr file.
+        """
+        self._write_zarr(zarr_file, chunks, write_chunk_zarr(zarr_file))
+
+    def write_zarr_gcs(self, gcs_path, chunks, gcs_project, gcs_token='cloud'):
+        """
+        Write an anndata object to a Zarr file on GCS.
+        """
+        import gcsfs.mapping
+        gcs = gcsfs.GCSFileSystem(gcs_project, token=gcs_token)
+        store = gcsfs.mapping.GCSMap(gcs_path, gcs=gcs)
+        self._write_zarr(store, chunks, write_chunk_zarr_gcs(gcs_path, gcs_project, gcs_token))
 
     def copy(self):
         return AnnDataRdd(self.adata.copy(), self.rdd)
